212357930133d4f791d11b3a7224c44a3d259bcb
James Cletus Dcruz <72412570+Jcletusdcruz6992@users.noreply.github.com>
2024-01-19 23:04:00 -0500

    made some changes on word counting code

--- c_program_to_count_number_of_words_characters_number_of_lines_from_a_input_string/main.c
+++ c_program_to_count_number_of_words_characters_number_of_lines_from_a_input_string/main.c
@@ -2,22 +2,22 @@
 #define IN 1     //IN means inside a word
 #define OUT 0    // OUT means outside a word
 int main(){
-char input_string[100];
+//char input_string[100];
 int nc,nw,state,nl;  //nc-number of characters , nw-number of words,state-either in or out , nl-number of lines
 int c;
 
 
 printf("Enter your input string \n");
 //scanf("%100s",input_string);      scanf doesnt read spaces
-fgets(input_string,sizeof(input_string),stdin);     // reads a line with spaces
-printf("String you entered is %s \n",input_string);
+//fgets(input_string,sizeof(input_string),stdin);     // reads a line with spaces
+//printf("String you entered is %s \n",input_string);
 
 nc=nw=nl=0;
 state=OUT;
-for(int i=0;input_string[i]!='\0';++i){
-        c=input_string[i];
+//for(int i=0;input_string[i]!='\0';++i){
+while((c=getchar())!=EOF){
         ++nc;
-    if(c=='\n'){
+    if(c=='\n' ){
         ++nl;
     }
     if(c=='\t' || c==' ' || c=='\n'){
