62dd8466db4bd046aaed45d32b8df627a165351c
James Cletus Dcruz <72412570+Jcletusdcruz6992@users.noreply.github.com>
2024-01-19 22:45:25 -0500

    adding program that counts number of words,lines,characters from an input string

---
+++ c_program_to_count_number_of_words_characters_number_of_lines_from_a_input_string/main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define IN 1     //IN means inside a word
+#define OUT 0    // OUT means outside a word
+int main(){
+char input_string[100];
+int nc,nw,state,nl;  //nc-number of characters , nw-number of words,state-either in or out , nl-number of lines
+int c;
+
+
+printf("Enter your input string \n");
+//scanf("%100s",input_string);      scanf doesnt read spaces
+fgets(input_string,sizeof(input_string),stdin);     // reads a line with spaces
+printf("String you entered is %s \n",input_string);
+
+nc=nw=nl=0;
+state=OUT;
+for(int i=0;input_string[i]!='\0';++i){
+        c=input_string[i];
+        ++nc;
+    if(c=='\n'){
+        ++nl;
+    }
+    if(c=='\t' || c==' ' || c=='\n'){
+        state=OUT;
+    }
+    else if(state==OUT){
+            state=IN;
+            ++nw;
+    }
+}
+printf("Nc=%d ,nl=%d , nw=%d \n",nc,nl,nw);
+return 0;
+}
---
+++ copies_input_to_output_replace_tab_with_slash_t_and_backspace_with_slash_b/main.c
@@ -0,0 +1,25 @@
+/*Write a program to copy its input to its output, replacing each tab by \t, each
+backspace by \b, and each backslash by \\. This makes tabs and backspaces visible in an
+unambiguous way.*/
+
+#include<stdio.h>
+
+int main(){
+int c;
+while((c=getchar())!=EOF){
+    if(c=='\t'){
+        putchar("\\\t");
+    }
+    if(c=='\b'){
+        putchar("\\\b");
+    }
+    if(c=='\\'){
+        putchar("\\\\");
+    }
+    else{
+        putchar(c);
+    }
+}
+
+return 0;
+}
